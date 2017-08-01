b65e04178b6029cff6ceb8be8342692e7c54b239
plepliple <piconifelipe@gmail.com>
2017-07-31 23:38:59 -0300

    Configurando Heroku

---
+++ config/Migrations/20170514214041_Users.php
@@ -0,0 +1,31 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Users extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+
+        $this->table('users')
+            ->addColumn('username', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+
+            ->create();
+
+    }
+}
